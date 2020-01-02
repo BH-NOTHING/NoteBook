f59077fdc0c8eb30a59663bf3585c91029888102
Jordan <wangyibo@ih5.cn>
2020-01-02 14:33:27 +0800

    feat: add editor

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <NoteEditor/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import NoteEditor from './components/editor'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NoteEditor
   }
 }
 </script>
---
+++ src/components/editor.vue
@@ -0,0 +1,102 @@
+<template>
+    <quill-editor
+      ref="myQuillEditor"
+      v-model="content"
+      :options="editorOption"
+      :disabled="!disabled"
+      contenteditable="false"
+      @blur="onEditorBlur($event)"
+      @focus="onEditorFocus($event)"
+      @ready="onEditorReady($event)"
+      @change="onEditorChange($event)"/>
+</template>
+
+<script>
+import quillEditor from 'vue-quill-editor/src/editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+export default {
+  name: 'noteEditor',
+  components: {
+    quillEditor
+  },
+  props: {
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '300px'
+    },
+    headColor: String,
+    bodyColor: String,
+    disabled: {
+      default: true,
+      type: Boolean
+    },
+    contentHtml: null,
+    version: {
+      type: Boolean,
+      default: false
+    },
+    setContent: String,
+    borderWidth: {
+      type: Number,
+      default: 1
+    },
+    borderColor: {
+      type: String,
+      default: '#CCCCCC'
+    },
+    placeholder: {
+      type: String,
+      default: '请输入内容'
+    }
+  },
+  data () {
+    return {
+      content: '',
+      editorOption: {},
+      code: false,
+      name: 'editor',
+      reset: false
+    }
+  },
+  computed: {
+    editor () {
+      return this.$refs.myQuillEditor.quill
+    }
+  },
+  created () {
+  },
+  updated () {
+    // this.updateStyle();
+  },
+  mounted () {
+  },
+  methods: {
+    onEditorBlur (quill) {
+      this.$emit('blur', {})
+    },
+    onEditorFocus (quill) {
+      this.$emit('focus', {})
+    },
+    onEditorReady (quill) {
+      this.$emit('ready', {})
+    },
+    onEditorChange ({ quill, html, text }) {
+      this.$emit('valueChange', {
+        contentHtml: JSON.stringify(quill.getContents().ops)
+      })
+    },
+    onEditorMouseover (e) {
+      this.$emit('mouseover', {})
+    },
+    onEditorMouseout (e) {
+      this.$emit('mouseout', {})
+    }
+  }
+}
+</script>
